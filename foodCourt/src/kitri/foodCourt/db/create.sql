f3d102e586d178ad16309c8ef7bfea22bb938f64
jwleejwlee <tsactiont@gmail.com>
2019-04-15 17:29:12 +0900

    19.04.15	Add menu image and test vectors

--- foodCourt/src/kitri/foodCourt/db/create.sql
+++ foodCourt/src/kitri/foodCourt/db/create.sql
@@ -402,11 +402,24 @@ insert into fook_job(job_id, job_name, grade_level) values ('Newbie', '신입', 'L
 
 -- MANAGER
 insert into fook_manager(manager_id, name, password, phone_first, phone_middle, phone_last, job_id, hire_date, address_zip, address, email, email_domain)
-values ('Admin123', '홍길동', '123qwe', '010', '1234', '5678', 'Admin', sysdate, '34343', 'Seoul yeouido', 'kitri', 'naver.com');
+values ('Administrator', '홍길동', '123q#we', '010', '1234', '9876', 'Admin', sysdate, '34343', '서울특별시 여의도동 영등포구 국회대로 17길', 'honggildong', 'naver.com');
 insert into fook_manager(manager_id, name, password, phone_first, phone_middle, phone_last, job_id, hire_date, address_zip, address, email, email_domain)
-values ('Clerk123', '서생원', '1a2s3d4f', '010', '2345', '6789', 'Clerk', sysdate, '43438', 'Seoul yeongdeungpogu', 'c234', 'hanmail.net');
+values ('Clerk1', '서생원', 'eidud928', '010', '2345', '8765', 'Clerk', sysdate, '43378', '경기도 시흥시 정왕동 254번지', 'seosaengwon', 'hanmail.net');
 insert into fook_manager(manager_id, name, password, phone_first, phone_middle, phone_last, job_id, hire_date, address_zip, address, email, email_domain)
-values ('Newbie123', '김두한', 'skujhf723', '010', '3456', '7890', 'Newbie', sysdate, '29843', 'Seoul jonglogu', '2396sdf', 'google.com');
+values ('Clerk2', '이방헌', 'cjkjd46!2', '010', '3456', '7654', 'Clerk', sysdate, '67890', '강원도 삼척시 도계읍 점리', 'leebangheon', 'naver.com');
+insert into fook_manager(manager_id, name, password, phone_first, phone_middle, phone_last, job_id, hire_date, address_zip, address, email, email_domain)
+values ('Clerk3', '정도전', 'djsd984u24s', '010', '4567', '6543', 'Clerk', sysdate, '25727', '충청북도 청주시 상당구 내덕동 173-6', 'jeongdojeon', 'google.com');
+insert into fook_manager(manager_id, name, password, phone_first, phone_middle, phone_last, job_id, hire_date, address_zip, address, email, email_domain)
+values ('Clerk4', '김구', 'kkjfg%$sk', '010', '5678', '5432', 'Clerk', sysdate, '92783', '전라남도 순천군 동초면 금치리', 'kimku', 'hanmail.net');
+insert into fook_manager(manager_id, name, password, phone_first, phone_middle, phone_last, job_id, hire_date, address_zip, address, email, email_domain)
+values ('Clerk5', '박상면', 'sdjsjf98248', '010', '6789', '4321', 'Clerk', sysdate, '23985', '제주도 서귀포시 성산읍 서성일로 433', 'parksangmyeon', 'google.com');
+insert into fook_manager(manager_id, name, password, phone_first, phone_middle, phone_last, job_id, hire_date, address_zip, address, email, email_domain)
+values ('Newbie1', '이순재', 'zgzg761', '010', '7890', '3210', 'Newbie', sysdate, '86748', '대전광역시 유성구 가정로 218', 'leesoonjae', 'google.com');
+insert into fook_manager(manager_id, name, password, phone_first, phone_middle, phone_last, job_id, hire_date, address_zip, address, email, email_domain)
+values ('Newbie2', '신구', 'sfju394f89', '010', '8901', '2109', 'Newbie', sysdate, '64921', '대구광역시 동구 동부로 459', 'shinku', 'naver.com');
+insert into fook_manager(manager_id, name, password, phone_first, phone_middle, phone_last, job_id, hire_date, address_zip, address, email, email_domain)
+values ('Newbie3', '김두한', 'kudfg908dws', '010', '9012', '1098', 'Newbie', sysdate, '10198', '울산광역시 북구 효문동 587-1', 'kimduhan', 'google.com');
+
 
 -- CATEGORY
 insert into fook_category(category_id, category_name) values (1, '한식');
@@ -416,13 +429,45 @@ insert into fook_category(category_id, category_name) values (4, '양식');
 
 -- FOOD
 insert into fook_food(food_id, food_name, category_id, price, food_point, food_description, image_address, manager_id, create_date, food_enable)
-values (food_fid_seq.nextval, '볶음밥', 1, 6000, 60, '신선한 야채를 사용.', 'bokeumbap.jpg', 'Clerk123', sysdate, 'y');
+values (food_fid_seq.nextval, '볶음밥', 1, 7000, 70, '신선한 야채를 사용.', 'bokeumbap.jpg', 'Newbie1', sysdate, 'y');
 insert into fook_food(food_id, food_name, category_id, price, food_point, food_description, image_address, manager_id, create_date, food_enable)
-values (food_fid_seq.nextval, '자장면', 2, 5000, 50, '직접 수타로 만듦.', 'jajangmyeon.jpg', 'Clerk123', sysdate, 'y');
+values (food_fid_seq.nextval, '안동국시', 1, 7500, 75, '안종 지역에서 유래한 국수.', 'andongkuksi.jpg', 'Newbie2', sysdate, 'y');
 insert into fook_food(food_id, food_name, category_id, price, food_point, food_description, image_address, manager_id, create_date, food_enable)
-values (food_fid_seq.nextval, '소바', 3, 7000, 70, '메밀이 들어감.', 'soba.jpg', 'Clerk123', sysdate, 'y');
+values (food_fid_seq.nextval, '바지락칼국수', 1, 7000, 70, '바지락이 들어가 개운함.', 'bajirakcalkuksu.jpg', 'Newbie3', sysdate, 'y');
 insert into fook_food(food_id, food_name, category_id, price, food_point, food_description, image_address, manager_id, create_date, food_enable)
-values (food_fid_seq.nextval, '스테이크', 4, 9500, 95, '뉴질랜드산 소고기를 사용.', 'steak.jpg', 'Admin123', sysdate, 'y');
+values (food_fid_seq.nextval, '뚝배기불고기', 1, 7900, 79, '기사식당의 인기 메뉴.', 'ddookbaegibulgogi.jpg', 'Clerk1', sysdate, 'y');
+insert into fook_food(food_id, food_name, category_id, price, food_point, food_description, image_address, manager_id, create_date, food_enable)
+values (food_fid_seq.nextval, '돌솥비빔밥', 1, 7500, 75, '다먹을 떄까지 온기가 유지됨.', 'dolsotbibimbap.jpg', 'Clerk2', sysdate, 'y');
+insert into fook_food(food_id, food_name, category_id, price, food_point, food_description, image_address, manager_id, create_date, food_enable)
+values (food_fid_seq.nextval, '갈비탕', 1, 9000, 90, '국내산 1등급 한우를 사용.', 'galbitang.jpg', 'Administrator', sysdate, 'y');
+insert into fook_food(food_id, food_name, category_id, price, food_point, food_description, image_address, manager_id, create_date, food_enable)
+values (food_fid_seq.nextval, '자장면', 2, 5000, 50, '직접 수타로 만듦.', 'jajangmyeon.jpg', 'Clerk4', sysdate, 'y');
+insert into fook_food(food_id, food_name, category_id, price, food_point, food_description, image_address, manager_id, create_date, food_enable)
+values (food_fid_seq.nextval, '마파두부', 2, 7000, 70, '중국식 두부 요리.', 'mapatofu.jpg', 'Clerk3', sysdate, 'y');
+insert into fook_food(food_id, food_name, category_id, price, food_point, food_description, image_address, manager_id, create_date, food_enable)
+values (food_fid_seq.nextval, '잡채밥', 2, 8000, 80, '짭잘한 잡채 요리.', 'japchaebap.jpg', 'Clerk5', sysdate, 'y');
+insert into fook_food(food_id, food_name, category_id, price, food_point, food_description, image_address, manager_id, create_date, food_enable)
+values (food_fid_seq.nextval, '찹쌀탕수육', 2, 13000, 130, '찹쌀로 만들어 식감이 쫄깃함.', 'chapssaltangsuyook.jpg', 'Clerk5', sysdate, 'y');
+insert into fook_food(food_id, food_name, category_id, price, food_point, food_description, image_address, manager_id, create_date, food_enable)
+values (food_fid_seq.nextval, '짬뽕', 2, 8000, 80, '칼칼한 국물이 일품임.', 'jjambbong.jpg', 'Clerk2', sysdate, 'y');
+insert into fook_food(food_id, food_name, category_id, price, food_point, food_description, image_address, manager_id, create_date, food_enable)
+values (food_fid_seq.nextval, '소바', 3, 7000, 70, '메밀이 들어감.', 'soba.jpg', 'Clerk1', sysdate, 'y');
+insert into fook_food(food_id, food_name, category_id, price, food_point, food_description, image_address, manager_id, create_date, food_enable)
+values (food_fid_seq.nextval, '알밥', 3, 8000, 80, '입안에서 톡톡 터지는 식감이 일품임.', 'albap.jpg', 'Clerk1', sysdate, 'y');
+insert into fook_food(food_id, food_name, category_id, price, food_point, food_description, image_address, manager_id, create_date, food_enable)
+values (food_fid_seq.nextval, '돈까스', 3, 8500, 85, '국내산 돈육을 사용함.', 'donkats.jpg', 'Clerk3', sysdate, 'y');
+insert into fook_food(food_id, food_name, category_id, price, food_point, food_description, image_address, manager_id, create_date, food_enable)
+values (food_fid_seq.nextval, '가쓰오우동', 3, 5000, 50, '정통 일본식 우동.', 'gasoudong.jpg', 'Clerk4', sysdate, 'y');
+insert into fook_food(food_id, food_name, category_id, price, food_point, food_description, image_address, manager_id, create_date, food_enable)
+values (food_fid_seq.nextval, '참치회덮밥', 3, 18000, 180, '참치 100% 사용.', 'chamchihoideopbap.jpg', 'Newbie2', sysdate, 'y');
+insert into fook_food(food_id, food_name, category_id, price, food_point, food_description, image_address, manager_id, create_date, food_enable)
+values (food_fid_seq.nextval, '스테이크', 4, 9500, 95, '뉴질랜드산 소고기를 사용.', 'steak.jpg', 'Administrator', sysdate, 'y');
+insert into fook_food(food_id, food_name, category_id, price, food_point, food_description, image_address, manager_id, create_date, food_enable)
+values (food_fid_seq.nextval, '토마토김치스파게티', 4, 8500, 85, '토마토와 김치의 만남.', 'tomatokimchispaghetti.jpg', 'Clerk2', sysdate, 'y');
+insert into fook_food(food_id, food_name, category_id, price, food_point, food_description, image_address, manager_id, create_date, food_enable)
+values (food_fid_seq.nextval, '클럽샌드위치', 4, 9000, 90, '한끼 식사로 적당함.', 'clubsandwitch.jpg', 'Newbie1', sysdate, 'y');
+insert into fook_food(food_id, food_name, category_id, price, food_point, food_description, image_address, manager_id, create_date, food_enable)
+values (food_fid_seq.nextval, '자이언트피자', 4, 14000, 140, '3~4인이 먹기에 적당한 피자.', 'giantpizza.jpg', 'Administrator', sysdate, 'y');
 INSERT INTO "FOOK"."FOOK_FOOD" (FOOD_ID, FOOD_NAME, CATEGORY_ID, PRICE, FOOD_POINT, FOOD_DESCRIPTION, IMAGE_ADDRESS, MANAGER_ID, CREATE_DATE, FOOD_ENABLE) VALUES ('6', '테스트1', '1', '6000', '60', '국내산 콩 사용1', 'ilsic.jpg', 'Admin123', TO_DATE('2019-04-12 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), 'y');
 INSERT INTO "FOOK"."FOOK_FOOD" (FOOD_ID, FOOD_NAME, CATEGORY_ID, PRICE, FOOD_POINT, FOOD_DESCRIPTION, IMAGE_ADDRESS, MANAGER_ID, CREATE_DATE, FOOD_ENABLE) VALUES ('7', '테스트2', '2', '6000', '60', '국내산 콩 사용2', 'jajangmyeon.jpg', 'Admin123', TO_DATE('2019-04-12 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), 'y');
 INSERT INTO "FOOK"."FOOK_FOOD" (FOOD_ID, FOOD_NAME, CATEGORY_ID, PRICE, FOOD_POINT, FOOD_DESCRIPTION, IMAGE_ADDRESS, MANAGER_ID, CREATE_DATE, FOOD_ENABLE) VALUES ('8', '테스트3', '3', '6000', '60', '국내산 콩 사용3', 'joongsic.jpg', 'Admin123', TO_DATE('2019-04-12 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), 'y');
@@ -440,7 +485,17 @@ values ('dfkjhg987', 'dfjhjh2', '박진영', '010', '3456', '7890', 10000, 'City?',
 insert into fook_user(user_id, password, name, phone_first, phone_middle, phone_last, user_point, password_quiz, password_answer, join_date, secession_date, enable)
 values ('dxcvowej34', '2309fjsdjfh', '양현석', '010', '7948', '2948', 300, 'Nation?', 'East Asia', sysdate, sysdate, 'n');
 insert into fook_user(user_id, password, name, phone_first, phone_middle, phone_last, user_point, password_quiz, password_answer, join_date, secession_date, enable)
-values ('calubang', '123456', '안병욱', '010', '7163', '3863', 5000, 'Nation?', 'East Asia', sysdate, sysdate, 'y');
+values ('jb287dhcb', 'drtlse@#', '서장훈', '010', '3489', '5666', 3000, 'First Number?', '010', sysdate, sysdate, 'n');
+insert into fook_user(user_id, password, name, phone_first, phone_middle, phone_last, user_point, password_quiz, password_answer, join_date, secession_date, enable)
+values ('sosuf7548', 'dj34897', '우지원', '010', '2907', '8956', 8000, 'Second Number?', '2907', sysdate, NULL, 'y');
+insert into fook_user(user_id, password, name, phone_first, phone_middle, phone_last, user_point, password_quiz, password_answer, join_date, secession_date, enable)
+values ('dh48dh', 'cjcjsd@#$', '단테존슨', '010', '7455', '2348', 1500, 'Last NUmber?', '2348', sysdate, NULL, 'y');
+insert into fook_user(user_id, password, name, phone_first, phone_middle, phone_last, user_point, password_quiz, password_answer, join_date, secession_date, enable)
+values ('sdfg9s82', 'sjsddgsd87d', '라건아', '010', '7356', '9034', 4200, 'Job?', 'Basketball player', sysdate, NULL, 'y');
+insert into fook_user(user_id, password, name, phone_first, phone_middle, phone_last, user_point, password_quiz, password_answer, join_date, secession_date, enable)
+values ('xx8z72', 'bbkg9048', '허재', '010', '0389', '2361', 9300, 'How many children?', '2 childs', sysdate, NULL, 'y');
+insert into fook_user(user_id, password, name, phone_first, phone_middle, phone_last, user_point, password_quiz, password_answer, join_date, secession_date, enable)
+values ('calubang', '123456', '안병욱', '010', '8927', '6383', 5000, 'Nation?', 'East Asia', sysdate, sysdate, 'y');
 
 -- PAYMENT
 insert into fook_payment(payment_id, user_id, payment_date, request_number, total_price, save_point, used_point, card, cash, payment_state)
@@ -449,27 +504,76 @@ insert into fook_payment(payment_id, user_id, payment_date, request_number, tota
 values (payment_pid_seq.nextval, 'dfkjhg987', sysdate, '0002', 150000, 1500, 0, 125000, 25000, '0');
 insert into fook_payment(payment_id, user_id, payment_date, request_number, total_price, save_point, used_point, card, cash, payment_state)
 values (payment_pid_seq.nextval, 'dxcvowej34', sysdate, '0003', 80000, 800, 0, 25000, 55000, '0');
+insert into fook_payment(payment_id, user_id, payment_date, request_number, total_price, save_point, used_point, card, cash, payment_state)
+values (payment_pid_seq.nextval, 'jb287dhcb', sysdate, '0004', 70000, 700, 5000, 10000, 55000, '0');
+insert into fook_payment(payment_id, user_id, payment_date, request_number, total_price, save_point, used_point, card, cash, payment_state)
+values (payment_pid_seq.nextval, 'sosuf7548', sysdate, '0005', 105000, 1050, 10000, 85000, 10000, '0');
+insert into fook_payment(payment_id, user_id, payment_date, request_number, total_price, save_point, used_point, card, cash, payment_state)
+values (payment_pid_seq.nextval, 'dh48dh', sysdate, '0006', 10000, 100, 0, 10000, 0, '0');
+insert into fook_payment(payment_id, user_id, payment_date, request_number, total_price, save_point, used_point, card, cash, payment_state)
+values (payment_pid_seq.nextval, 'sdfg9s82', sysdate, '0007', 66500, 665, 1000, 60000, 5000, '0');
+insert into fook_payment(payment_id, user_id, payment_date, request_number, total_price, save_point, used_point, card, cash, payment_state)
+values (payment_pid_seq.nextval, 'xx8z72', sysdate, '0008', 78000, 780, 8000, 15000, 55000, '0');
 
 -- PAYMENT_DETAIL
 insert into fook_payment_detail(payment_id, food_id, food_name, price, count, point)
-values ('1', '72', '볶음밥', 6000, 3, 60);
+values ('0', '7', '자장면', 5000, 3, 50);
 insert into fook_payment_detail(payment_id, food_id, food_name, price, count, point)
-values ('1', '73', '자장면', 5000, 2, 50);
-insert into fook_payment_detail(payment_id, food_id, food_name, price, count, point)
-values ('1', '74', '소바', 7000, 1, 70);
-insert into fook_payment_detail(payment_id, food_id, food_name, price, count, point)
-values ('1', '75', '스테이크', 9500, 4, 95);
+values ('0', '12', '소바', 7000, 2, 70);
 
 insert into fook_payment_detail(payment_id, food_id, food_name, price, count, point)
-values ('0', '73', '자장면', 5000, 3, 50);
+values ('1', '1', '볶음밥', 6000, 3, 60);
 insert into fook_payment_detail(payment_id, food_id, food_name, price, count, point)
-values ('0', '74', '소바', 7000, 2, 70);
+values ('1', '7', '자장면', 5000, 2, 50);
+insert into fook_payment_detail(payment_id, food_id, food_name, price, count, point)
+values ('1', '12', '소바', 7000, 1, 70);
+insert into fook_payment_detail(payment_id, food_id, food_name, price, count, point)
+values ('1', '17', '스테이크', 9500, 4, 95);
 
 insert into fook_payment_detail(payment_id, food_id, food_name, price, count, point)
-values ('2', '72', '볶음밥', 6000, 5, 60);
+values ('2', '1', '볶음밥', 6000, 5, 60);
 insert into fook_payment_detail(payment_id, food_id, food_name, price, count, point)
-values ('2', '74', '소바', 7000, 3, 70);
+values ('2', '12', '소바', 7000, 3, 70);
 insert into fook_payment_detail(payment_id, food_id, food_name, price, count, point)
-values ('2', '75', '스테이크', 9500, 1, 95);
+values ('2', '17', '스테이크', 9500, 1, 95);
+
+insert into fook_payment_detail(payment_id, food_id, food_name, price, count, point)
+values ('3', '2', '안동국시', 7500, 3, 75);
+insert into fook_payment_detail(payment_id, food_id, food_name, price, count, point)
+values ('3', '19', '클럽샌드위치', 9000, 2, 90);
+insert into fook_payment_detail(payment_id, food_id, food_name, price, count, point)
+values ('3', '11', '짬뽕', 8000, 1, 80);
+insert into fook_payment_detail(payment_id, food_id, food_name, price, count, point)
+values ('3', '8', '마파두부', 7000, 2, 70);
+insert into fook_payment_detail(payment_id, food_id, food_name, price, count, point)
+values ('3', '5', '돌솥비빔밥', 7500, 4, 75);
+insert into fook_payment_detail(payment_id, food_id, food_name, price, count, point)
+values ('3', '10', '찹쌀탕수육', 13000, 1, 130);
+
+insert into fook_payment_detail(payment_id, food_id, food_name, price, count, point)
+values ('4', '16', '참치회덮밥', 18000, 1, 180);
+
+insert into fook_payment_detail(payment_id, food_id, food_name, price, count, point)
+values ('5', '16', '토마토김치스파게티', 8500, 1, 85);
+insert into fook_payment_detail(payment_id, food_id, food_name, price, count, point)
+values ('5', '9', '잡채밥', 8000, 3, 80);
+insert into fook_payment_detail(payment_id, food_id, food_name, price, count, point)
+values ('5', '5', '돌솥비빔밥', 7500, 2, 75);
+insert into fook_payment_detail(payment_id, food_id, food_name, price, count, point)
+values ('5', '4', '뚝배기불고기', 7900, 1, 79);
+
+insert into fook_payment_detail(payment_id, food_id, food_name, price, count, point)
+values ('6', '14', '돈까스', 8500, 3, 85);
+insert into fook_payment_detail(payment_id, food_id, food_name, price, count, point)
+values ('6', '15', '가쓰오우동', 5000, 4, 50);
+
+insert into fook_payment_detail(payment_id, food_id, food_name, price, count, point)
+values ('7', '11', '짬뽕', 8000, 3, 80);
+insert into fook_payment_detail(payment_id, food_id, food_name, price, count, point)
+values ('7', '13', '알밥', 8000, 1, 80);
+
+insert into fook_payment_detail(payment_id, food_id, food_name, price, count, point)
+values ('8', '3', '바지락칼국수', 7000, 8, 70);
+
 
 COMMIT;
