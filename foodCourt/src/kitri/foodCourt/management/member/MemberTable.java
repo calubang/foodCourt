07b85f2639387b2508ea0de99c043befb840fa38
calubang <acalubang@gmail.com>
2019-04-15 17:59:43 +0900

    19.04.15 abo update
    memverTable update

--- foodCourt/src/kitri/foodCourt/management/member/MemberTable.java
+++ foodCourt/src/kitri/foodCourt/management/member/MemberTable.java
@@ -2,83 +2,39 @@ package kitri.foodCourt.management.member;
 
 import java.awt.Dimension;
 import java.sql.*;
+import java.util.Vector;
 
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
+import kitri.foodCourt.dto.UserDto;
+import kitri.foodCourt.management.regit.AdminUserDao;
 import kitri.foodCourt.management.regit.DBConnection;
+import kitri.foodCourt.user.swing.SwingFactory;
 
 public class MemberTable extends JPanel {
 
 	JScrollPane scrollPane = new JScrollPane();
-	DefaultTableModel dtm = new DefaultTableModel();
-	private JTable memberTable = new JTable(dtm);
+	DefaultTableModel tableModel = new DefaultTableModel();
+	private JTable memberTable = new JTable(tableModel);
+	private AdminUserDao dao;
 	
-	Connection conn = null;
-	PreparedStatement pstm = null;
-	ResultSet rs = null;
+	String[] column = { "유저ID", "패스워드", "이름", "핸드폰번호", "사용가능포인트", "비밀번호 찾기용질문", "비밀번호 찾기용답변",  "가입일", "탈퇴일", "활성화여부" };
 	
-	String[] column = { "유저ID", "이름", "핸드폰번호", "사용가능포인트", "가입일", "탈퇴일", "활성화여부" };
-	/**
-	 * Create the panel.
-	 */
 	public MemberTable() {
-		Object[] rowData = new Object[11];
-		try {
-
-			String quary = "SELECT user_id,password,phone_first,phone_middle,phone_last,user_point,password_quiz,password_answer,join_date,secession_date,enable FROM FOOK_USER";
-			conn = DBConnection.getConnection();
-			pstm = conn.prepareStatement(quary);
-			rs = pstm.executeQuery();
-			
-			
-			
-			while (rs.next()) {
-
-				rowData[0] = rs.getString("user_id");
-				rowData[1] = rs.getString("password");
-				rowData[2] = rs.getString("phone_first");
-				rowData[3] = rs.getString("phone_middle");
-				rowData[4] = rs.getString("phone_last");
-				rowData[5] = rs.getInt("user_point");
-				rowData[6] = rs.getString("password_quiz");
-				rowData[7] = rs.getString("password_answer");
-				rowData[8] = rs.getDate("join_date");
-				rowData[9] = rs.getDate("secession_date");
-				rowData[10] = rs.getString("enable");
-				
-				
-				dtm.addRow(rowData);
-			
-			}
-			
-		} catch (SQLException sqle) {
-			System.out.println("SELECT문에서 예외 발생");
-			sqle.printStackTrace();
-
-		} finally {
-			// DB 연결을 종료한다.
-			try {
-				if (rs != null) {
-					rs.close();
-				}
-				if (pstm != null) {
-					pstm.close();
-				}
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (Exception e) {
-				throw new RuntimeException(e.getMessage());
-			}
-			
-		}
 		
+		dao = new AdminUserDao();
+		initView();
 		
+		initData();
+		
+	}
+	
+	public void initView() {
 		for (int i = 0; i < column.length; i++) {
-			dtm.addColumn(column[i]);
+			tableModel.addColumn(column[i]);
 		}
 		
 		setSize(new Dimension(780, 640));
@@ -87,6 +43,40 @@ public class MemberTable extends JPanel {
 		scrollPane.setBounds(0, 0, 780, 640);
 		scrollPane.setViewportView(memberTable);
 		add(scrollPane);
+	}
+	
+	public void initData() {
+		Vector<UserDto> vector = dao.allSelect();
+		
+		if(vector == null) {
+			SwingFactory.getOptionPane("errorMessage", memberTable, "DB오류", "DB연결 오류");
+			return;
+		} else {
+			insertTable(vector);
+		}
+	}
+	
+	public void insertTable(Vector<UserDto> vector) {
+		
+		int size = vector.size();
+		for(int i=0 ; i<size ; i++) {
+			Vector<Object> temp = new Vector<Object>();
+			//{ "유저ID", "패스워드", "이름", "핸드폰번호", "사용가능포인트", "비밀번호 찾기용질문", "비밀번호 찾기용답변",  "가입일", "탈퇴일", "활성화여부" };
+			UserDto dto = vector.get(i);
+			temp.add(dto.getUserId());
+			temp.add(dto.getPassword());
+			temp.add(dto.getName());
+			temp.add(dto.getPhoneNumberFirst() + "-" + dto.getPhoneNumberMiddle() + "-" + dto.getPhoneNumberlast());
+			temp.add(dto.getUserPoint());
+			temp.add(dto.getPasswordQuiz());
+			temp.add(dto.getPasswordAnswer());
+			temp.add(dto.getJoinDate());
+			temp.add(dto.getSecessionDate());
+			temp.add(dto.getEnable());
+			
+			tableModel.addRow(temp);
+			
+		}
 	}
 
 }
